471a009da42d82e690ed13f176b05d63ebdaa638
Happy2018new <chenbo79800@163.com>
2024-02-19 18:01:19 +0800

    use shell script to detect upstream change

---
+++ test_commit_changes.sh
@@ -0,0 +1,12 @@
+local_record=$(sed -n '1,1p' commit)
+remote_commit=$(sed -n '1,1p' neomega_kernel/.git/refs/remotes/origin/main)
+# prepare
+if [ $local_record != $remote_commit ]; then
+    echo "${remote_commit}" > commit
+    echo 'changed=true' >> $GITHUB_OUTPUT
+    # if changed, we sync the commit id and build depends
+else
+    echo 'changed=false' >> $GITHUB_OUTPUT
+    # if not changed, we do NOTHING
+fi
+# test commit changes
